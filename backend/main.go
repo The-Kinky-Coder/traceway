2249eab27c5e6b377ca70c908e8c70b5f820d269
ddux <dusan.stanojevic.cs@gmail.com>
2026-02-22 14:23:24 -0600

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -3,5 +3,13 @@ package main
 import "backend/cmd"
 
 func main() {
-	cmd.Run()
+	cmd.Run(nil)
+
+	// to run in embeded mode:
+	// cmd.Run(
+	// 	cmd.WithPort(8082),
+	// 	cmd.WithDefaultUser("admin@localhost.com", "admin"),
+	// 	cmd.WithDefaultProject("Backend", "go", "backend-dev-token"),
+	// 	cmd.WithDefaultProject("Frontend", "sveltekit", "frontend-dev-token"),
+	// )
 }
