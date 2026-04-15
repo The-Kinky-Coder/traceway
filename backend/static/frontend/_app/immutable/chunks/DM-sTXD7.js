b10deb460dc72358473991f5bffd231f686bf9b1
ddux <dusan.stanojevic.cs@gmail.com>
2026-04-15 12:27:36 -0500

    release: backend v1.7.4

--- backend/static/frontend/_app/immutable/chunks/DM-sTXD7.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./BAqy0k2f.js";function i(r,t){throw new e(r,t.toString())}export{i as r};
--- backend/static/frontend/_app/immutable/chunks/DceERAwB.js
+++
@@ -1,1 +0,0 @@
-import{b as r,r as o}from"./BAqy0k2f.js";const t="";function a(...e){return r+t+o(e[0],e[1])}export{a as r};
